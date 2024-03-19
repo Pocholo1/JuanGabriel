dd60e0ecfe58dbfb8ed57bb203c16c8189d3664b
Pocholo1 <juan0730j@gmail.com>
2024-03-19 16:50:29 -0500

    ProyectoLibros

---
+++ ProyectoDeLibros/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import CardList from './componentes/body/CardList'
+import Cards from './componentes/body/Cards'
+
+
+function App() {
+
+  return (
+   <div>
+    <header/>
+    <Cards/>
+    <CardList/>
+   </div>
+  )
+}
+
+export default App
---
+++ ProyectoDeLibros/src/componentes/body/data.jsx
@@ -0,0 +1,122 @@
+export default [
+    {
+        id:1,
+        title: "Cien años de soledad",
+        autor: "Gabriel García Márquez",
+        fechaLanzamiento:"20-12-1980",
+        description: "Cien años de soledad se ha convertido en la novela más emblemática de la literatura latinoamericana",
+        image: cienAños,
+        precio:"$1000"
+    },
+
+    {
+        id:2,
+        title: "COSMOS",
+        autor: "Carl Sagan",
+        fechaLanzamiento:"20-12-1980",
+        description: "Cosmos trata de la ciencia en un contexto humano más amplio y explica como la ciencia y la ciencia ",
+        image: cosmos,
+        precio:"$25000"
+    },
+
+    {
+        id:3,
+        title: "El coronel no tiene quien le escriba",
+        autor: "Gabriel Garcá Márquez",
+        fechaLanzamiento:"20-12-1979",
+        description: "La vida",
+        image: coronel,
+        precio:"$25000"
+    },
+
+    {
+        id:4,
+        title: "El amor en los tiempos de cólera",
+        autor: "Gabriel Garcá Márquez",
+        fechaLanzamiento:"20-12-1995",
+        description: "La vida",
+        image: colera,
+        precio:"$25000"
+    },
+
+    {
+        id:5,
+        title: "Todos los cuentos",
+        autor: "Gabriel Garcá Márquez",
+        fechaLanzamiento:"20-12-1995",
+        description: "La vida",
+        image: todosCuentos,
+        precio:"$25000"
+    },
+
+    {
+        id:6,
+        title: "Contacto",
+        autor: "Carl Sagan",
+        fechaLanzamiento:"20-12-1989",
+        description: "Contacto",
+        image: contacto,
+        precio:"$25000"
+    },
+
+    {
+        id:7,
+        title: "Cerebro de Broca",
+        autor: "Carl Sagan",
+        fechaLanzamiento:"20-12-1989",
+        description: "Contacto",
+        image: cerebroBroca,
+        precio:"$25000"
+    },
+
+    {
+        id:8,
+        title: "Los miserables",
+        autor: "Carl Sagan",
+        fechaLanzamiento:"20-12-1989",
+        description: "Contacto",
+        image: miserables,
+        precio:"$25000"
+    },
+
+    {
+        id:9,
+        title: "Los cuatro acuerdos",
+        autor: "Carl Sagan",
+        fechaLanzamiento:"20-12-1989",
+        description: "Contacto",
+        image: cuatroAcuerdos,
+        precio:"$25000"
+    },
+
+    {
+        id:10,
+        title: "La lógica de la mentira",
+        autor: "Carl Sagan",
+        fechaLanzamiento:"20-12-1989",
+        description: "Contacto",
+        image: logicaMentira,
+        precio:"$25000"
+    },
+
+    {
+        id:11,
+        title: "Diccionario de ajedrez",
+        autor: "Carl Sagan",
+        fechaLanzamiento:"20-12-1989",
+        description: "Contacto",
+        image: diccionarioAjedrez,
+        precio:"$25000"
+    },
+
+    {
+        id:12,
+        title: "El poder de las palabras",
+        autor: "Sigman Mariano",
+        fechaLanzamiento:"20-12-1989",
+        description: "Contacto",
+        image: poderPalabras,
+        precio:"$25000"
+    }
+
+]
